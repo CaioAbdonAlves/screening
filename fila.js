4ce52807ec6b5ab9e1f9c1151e4a77ba0b177ff8
Caio Abdon <a.caio@cicclo.com>
2024-05-28 18:38:25 -0300

    feat: Create method enqueue

--- fila.js
+++ fila.js
@@ -1,6 +1,19 @@
 class PriorityQueue {
     constructor() {
-        this.item = [];
+        this.items = [];
+    }
+
+    enqueue(item) {
+        if (!item || typeof item.name !== 'string' || typeof item.priority !== 'number') {
+            throw new Error('O item deve ter um nome e uma prioridade válida');
+        }
+
+        const index = this.items.findIndex((i) => i.priority > item.priority);
+        if (index === -1) {
+            this.items.push(item);
+        } else {
+            this.items.splice(index, 0, item);
+        }
     }
 }
 
