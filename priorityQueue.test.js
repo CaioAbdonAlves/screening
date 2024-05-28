a2f6c1f401a2172fb11eb5753c5b78230fae23b6
Caio Abdon <a.caio@cicclo.com>
2024-05-28 20:09:04 -0300

    test: Add test for enqueue method

--- priorityQueue.test.js
+++ priorityQueue.test.js
@@ -6,4 +6,16 @@ describe('PriorityQueue', () => {
     beforeEach(() => {
         queue = new PriorityQueue();
     });
+    
+    test('O método enqueue deve adicionar itens corretamente', () => {
+        queue.enqueue({ name: 'Gato', priority: 1 });
+        queue.enqueue({ name: 'Cachorro', priority: 4 });
+        queue.enqueue({ name: 'Coelho', priority: 2 });
+    
+        expect(queue.getItems()).toEqual([
+        { name: 'Gato', priority: 1 },
+        { name: 'Coelho', priority: 2 },
+        { name: 'Cachorro', priority: 4 }
+        ]);
+    });
 });
